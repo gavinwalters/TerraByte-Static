973e8893e9f977d5baceef8c43cc145987106672
gavinwalters <gavin.j.walters@gmail.com>
2023-11-13 20:36:37 -0500

    mobile responsive

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,8 +1,22 @@
-function toggleMenu() {
-  var navList = document.getElementById("navList");
-  if (navList.className === "responsive") {
-    navList.className = ""; // Remove the responsive class
-  } else {
-    navList.className = "responsive"; // Add the responsive class
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const xmark = document.querySelector(".xmark");
+  function lockScroll() {
+    document.body.classList.toggle("lock-scroll");
+    console.log("LOCK");
   }
-}
+
+  hamburger.addEventListener("click", function () {
+    mobileNav.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    xmark.classList.toggle("active");
+    lockScroll();
+  });
+  xmark.addEventListener("click", function () {
+    mobileNav.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    xmark.classList.toggle("active");
+    lockScroll();
+  });
+});
